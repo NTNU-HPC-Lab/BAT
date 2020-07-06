9b2f205cc9429cb409f9cb17ee1692b5751679f9
Ingunn Sund <ingunnsu@online.no>
2020-07-06 23:01:54 +0200

    Add MD5Hash program and OpenTuner implementation

---
+++ src/programs/md5hash/md5hash.cu
@@ -0,0 +1,355 @@
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include "OptionParser.h"
+#include "cudacommon.h"
+#include "md5hash_kernel.h"
+
+using namespace std;
+
+
+
+// ****************************************************************************
+// Function:  FindKeyspaceSize
+//
+// Purpose:
+///   Multiply out the byteLength by valsPerByte to find the 
+///   total size of the key space, with error checking.
+//
+// Arguments:
+//   byteLength    number of bytes in a key
+//   valsPerByte   number of values each byte can take on
+//
+// Programmer:  Jeremy Meredith
+// Creation:    July 23, 2014
+//
+// Modifications:
+// ****************************************************************************
+__host__ __device__ int FindKeyspaceSize(int byteLength, int valsPerByte)
+{
+    int keyspace = 1;
+    for (int i=0; i<byteLength; ++i)
+    {
+        if (keyspace >= 0x7fffffff / valsPerByte)
+        {
+            // error, we're about to overflow a signed int
+            return -1;
+        }
+        keyspace *= valsPerByte;
+    }
+    return keyspace;
+}
+
+
+
+
+// ****************************************************************************
+// Function:  AsHex
+//
+// Purpose:
+///   For a given key string, return the raw hex string for its bytes.
+//
+// Arguments:
+//   vals       key string
+//   len        length of key string
+//
+// Programmer:  Jeremy Meredith
+// Creation:    July 23, 2014
+//
+// Modifications:
+// ****************************************************************************
+std::string AsHex(unsigned char *vals, int len)
+{
+    ostringstream out;
+    char tmp[256];
+    for (int i=0; i<len; ++i)
+    {
+        sprintf(tmp, "%2.2X", vals[i]);
+        out << tmp;
+    }
+    return out.str();
+}
+
+
+
+// ****************************************************************************
+// Function: addBenchmarkSpecOptions
+//
+// Purpose:
+//   Add benchmark specific options parsing
+//
+// Arguments:
+//   op: the options parser / parameter database
+//
+// Returns:  nothing
+//
+// Programmer: Jeremy Meredith
+// Creation: July 23, 2014
+//
+// Modifications:
+//
+// ****************************************************************************
+void
+addBenchmarkSpecOptions(OptionParser &op)
+{
+}
+
+
+
+// ****************************************************************************
+// Function:  FindKeyWithDigest_GPU
+//
+// Purpose:
+///   On the GPU, search the key space to find a key with the given digest.
+//
+// Arguments:
+//   searchDigest    the digest to search for
+//   byteLength      number of bytes in a key
+//   valsPerByte     number of values each byte can take on
+//   foundIndex      output - the index of the found key (if found)
+//   foundKey        output - the string of the found key (if found)
+//   foundDigest     output - the digest of the found key (if found)
+//
+// Programmer:  Jeremy Meredith
+// Creation:    July 23, 2014
+//
+// Modifications:
+// ****************************************************************************
+double FindKeyWithDigest_GPU(const unsigned int searchDigest[4],
+                             const int byteLength,
+                             const int valsPerByte,
+                             int *foundIndex,
+                             unsigned char foundKey[8],
+                             unsigned int foundDigest[4])
+{
+    int keyspace = FindKeyspaceSize(byteLength, valsPerByte);
+
+    //
+    // allocate output buffers
+    //
+    int *d_foundIndex;
+    cudaMalloc((void**)&d_foundIndex, sizeof(int) * 1);
+    CHECK_CUDA_ERROR();
+    unsigned char *d_foundKey;
+    cudaMalloc((void**)&d_foundKey, 8);
+    CHECK_CUDA_ERROR();
+    unsigned int *d_foundDigest;
+    cudaMalloc((void**)&d_foundDigest, sizeof(unsigned int) * 4);
+    CHECK_CUDA_ERROR();
+
+    //
+    // initialize output buffers to show no found result
+    //
+    cudaMemcpy(d_foundIndex, foundIndex, sizeof(int) * 1, cudaMemcpyHostToDevice);
+    CHECK_CUDA_ERROR();
+    cudaMemcpy(d_foundKey, foundKey, 8, cudaMemcpyHostToDevice);
+    CHECK_CUDA_ERROR();
+    cudaMemcpy(d_foundDigest, foundDigest, sizeof(unsigned int) * 4, cudaMemcpyHostToDevice);
+    CHECK_CUDA_ERROR();
+
+    //
+    // calculate work thread shape
+    //
+    int nthreads = BLOCK_SIZE;
+    size_t nblocks  = ceil((double(keyspace) / double(valsPerByte)) / double(nthreads));
+
+    //
+    // run the kernel
+    //
+    FindKeyWithDigest_Kernel<<<nblocks, nthreads>>>(searchDigest[0],
+                                                    searchDigest[1],
+                                                    searchDigest[2],
+                                                    searchDigest[3],
+                                                    keyspace,
+                                                    byteLength, valsPerByte,
+                                                    d_foundIndex,
+                                                    d_foundKey,
+                                                    d_foundDigest);
+    CHECK_CUDA_ERROR();
+
+    //
+    // read the (presumably) found key
+    //
+    cudaMemcpy(foundIndex, d_foundIndex, sizeof(int) * 1, cudaMemcpyDeviceToHost);
+    CHECK_CUDA_ERROR();
+    cudaMemcpy(foundKey, d_foundKey, 8, cudaMemcpyDeviceToHost);
+    CHECK_CUDA_ERROR();
+    cudaMemcpy(foundDigest, d_foundDigest, sizeof(unsigned int) * 4, cudaMemcpyDeviceToHost);
+    CHECK_CUDA_ERROR();
+
+    //
+    // free device memory
+    //
+    cudaFree(d_foundIndex);
+    CHECK_CUDA_ERROR();
+    cudaFree(d_foundKey);
+    CHECK_CUDA_ERROR();
+    cudaFree(d_foundDigest);
+    CHECK_CUDA_ERROR();
+
+    //
+    // return the runtime in seconds
+    //
+    return 1.0;
+}
+
+
+// ****************************************************************************
+// Function: RunBenchmark
+//
+// Purpose:
+//   Executes the MD5 Hash benchmark
+//
+// Arguments:
+//   resultDB: results from the benchmark are stored in this db
+//   op: the options parser / parameter database
+//
+// Returns:  nothing
+//
+// Programmer: Jeremy Meredith
+// Creation: July 23, 2014
+//
+// Modifications:
+//
+// ****************************************************************************
+void
+RunBenchmark(OptionParser &op)
+{
+    bool verbose = op.getOptionBool("verbose");
+
+    int size = op.getOptionInt("size");
+    if (size < 1 || size > 4)
+    {
+        cerr << "ERROR: Invalid size parameter\n";
+        return;
+    }
+
+    //
+    // Determine the shape/size of key space
+    //
+    const int sizes_byteLength[]  = { 7,  5,  6,  5};
+    const int sizes_valsPerByte[] = {10, 35, 25, 70};
+
+    const int byteLength = sizes_byteLength[size-1];   
+    const int valsPerByte = sizes_valsPerByte[size-1];
+
+    char atts[1024];
+    sprintf(atts, "%dx%d", byteLength, valsPerByte);
+
+    if (verbose)
+        cout << "Searching keys of length " << byteLength << " bytes "
+             << "and " << valsPerByte << " values per byte" << endl;
+
+    const int keyspace = FindKeyspaceSize(byteLength, valsPerByte);
+    if (keyspace < 0)
+    {
+        cerr << "Error: more than 2^31 bits of entropy is unsupported.\n";
+        return;
+    }
+
+    if (byteLength > 7)
+    {
+        cerr << "Error: more than 7 byte key length is unsupported.\n";
+        return;
+    }
+
+    if (verbose)
+        cout << "|keyspace| = " << keyspace << " ("<<int(keyspace/1e6)<<"M)" << endl;
+
+    //
+    // Choose a random key from the keyspace, and calculate its hash.
+    //
+    //srandom(12345);
+    srandom(time(NULL));
+
+    int passes = op.getOptionInt("passes");
+
+    for (int pass = 0 ; pass < passes ; ++pass)
+    {
+        int randomIndex = random() % keyspace;;
+        unsigned char randomKey[8] = {0,0,0,0, 0,0,0,0};
+        unsigned int randomDigest[4];
+        IndexToKey(randomIndex, byteLength, valsPerByte, randomKey);
+        md5_2words((unsigned int*)randomKey, byteLength, randomDigest);
+
+        if (verbose)
+        {
+            cout << endl;
+            cout << "--- pass " << pass << " ---" << endl;
+            cout << "Looking for random key:" << endl;
+            cout << " randomIndex = " << randomIndex << endl;
+            cout << " randomKey   = 0x" << AsHex(randomKey, 8/*byteLength*/) << endl;
+            cout << " randomDigest= " << AsHex((unsigned char*)randomDigest, 16) << endl;
+        }
+
+        //
+        // Use the GPU to brute force search the keyspace for this key.
+        //
+        unsigned int foundDigest[4] = {0,0,0,0};
+        int foundIndex = -1;
+        unsigned char foundKey[8] = {0,0,0,0, 0,0,0,0};
+
+        // in seconds
+        double t = FindKeyWithDigest_GPU(randomDigest, byteLength, valsPerByte,
+            &foundIndex, foundKey, foundDigest);
+
+
+        //
+        // Double check everything matches (index, key, hash).
+        //
+        if (foundIndex < 0)
+        {
+            cerr << "\nERROR: could not find a match.\n";
+        }
+        else if (foundIndex != randomIndex)
+        {
+            cerr << "\nERROR: mismatch in key index found.\n";
+        }
+        else if (foundKey[0] != randomKey[0] ||
+            foundKey[1] != randomKey[1] ||
+            foundKey[2] != randomKey[2] ||
+            foundKey[3] != randomKey[3] ||
+            foundKey[4] != randomKey[4] ||
+            foundKey[5] != randomKey[5] ||
+            foundKey[6] != randomKey[6] ||
+            foundKey[7] != randomKey[7])
+        {
+            cerr << "\nERROR: mismatch in key value found.\n";
+        }        
+        else if (foundDigest[0] != randomDigest[0] ||
+            foundDigest[1] != randomDigest[1] ||
+            foundDigest[2] != randomDigest[2] ||
+            foundDigest[3] != randomDigest[3])
+        {
+            cerr << "\nERROR: mismatch in digest of key.\n";
+        }
+        else
+        {
+            if (verbose)
+                cout << endl << "Successfully found match (index, key, hash):" << endl;
+        }
+
+        //
+        // Add the calculated performancethe results
+        //
+
+        if (verbose)
+        {
+            cout << " foundIndex  = " << foundIndex << endl;
+            cout << " foundKey    = 0x" << AsHex(foundKey, 8/*byteLength*/) << endl;
+            cout << " foundDigest = " << AsHex((unsigned char*)foundDigest, 16) << endl;
+            cout << endl;
+        }
+    }
+
+    return;
+}
+
---
+++ tuning_examples/opentuner/md5hash/md5hash_tuner.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import opentuner
+from opentuner import ConfigurationManipulator
+from opentuner import IntegerParameter
+from opentuner import EnumParameter
+from opentuner.search.manipulator import BooleanParameter
+from opentuner import MeasurementInterface
+from opentuner import Result
+from numba import cuda
+import math
+
+start_path = '../../../src/programs'
+
+class MD5HashTuner(MeasurementInterface):
+
+    def manipulator(self):
+        """
+        Define the search space by creating a
+        ConfigurationManipulator
+        """
+        """
+        gpu = cuda.get_current_device()
+        sizes = [1000, 10000, 100000, 1000000, 10000000]
+        numVerts = sizes[argparser.parse_args().problem_size - 1]
+
+        min_size = 1
+        max_size = min(numVerts, gpu.MAX_THREADS_PER_BLOCK)
+        """
+        manipulator = ConfigurationManipulator()
+        #manipulator.add_parameter(IntegerParameter('BLOCK_SIZE', min_size, max_size))
+        #manipulator.add_parameter(EnumParameter('CHUNK_SIZE', [32, 64, 128, 256]))
+        manipulator.add_parameter(EnumParameter('BLOCK_SIZE', [252, 384]))
+        manipulator.add_parameter(EnumParameter('ROUND_STYLE', [0, 1]))
+
+
+        return manipulator
+
+    def run(self, desired_result, input, limit):
+        """
+        Compile and run a given configuration then
+        return performance
+        """
+        args = argparser.parse_args()
+
+        cfg = desired_result.configuration.data
+        compute_capability = cuda.get_current_device().compute_capability
+        cc = str(compute_capability[0]) + str(compute_capability[1])
+
+        make_program = 'nvcc -gencode=arch=compute_{0},code=sm_{0} -I {1}/cuda-common -I {1}/common -g -O2 -c {1}/md5hash/md5hash.cu'.format(cc, start_path)
+        make_program += ' -D{0}={1}'.format('ROUND_STYLE',cfg['ROUND_STYLE'])
+        make_program += ' -D{0}={1} \n'.format('BLOCK_SIZE',cfg['BLOCK_SIZE'])
+
+        if args.parallel:
+            make_paralell_start = 'mpicxx -I {0}/common/ -I {0}/cuda-common/ -I /usr/local/cuda/include -DPARALLEL -I {0}/mpi-common/ -g -O2 -c -o main.o {0}/cuda-common/main.cpp \n'.format(start_path)
+            make_paralell_end = 'mpicxx -L {0}/cuda-common -L {0}/common -o md5hash main.o md5hash.o -lSHOCCommon "-L/usr/local/cuda/bin/../targets/x86_64-linux/lib/stubs" "-L/usr/local/cuda/bin/../targets/x86_64-linux/lib" -lcudadevrt -lcudart_static -lrt -lpthread -ldl -lrt -lrt'.format(start_path)
+            compile_cmd = make_paralell_start + make_program + make_paralell_end
+        else:
+            make_serial_start = 'nvcc -I {0}/common/ -I {0}/cuda-common/ -g -O2 -c -o main.o {0}/cuda-common/main.cpp \n'.format(start_path)
+            make_serial_end = 'nvcc -L {0}/cuda-common -L {0}/common -o md5hash main.o md5hash.o -lSHOCCommon'.format(start_path)
+            compile_cmd = make_serial_start + make_program + make_serial_end
+    
+        compile_result = self.call_program(compile_cmd)
+        assert compile_result['returncode'] == 0
+
+        program_command = './md5hash -s ' + str(args.problem_size)
+        if args.parallel:
+            # Select number below max connected GPUs
+            chosen_gpu_number = min(args.gpu_num, len(cuda.gpus))
+      
+            devices = ','.join([str(i) for i in range(0, chosen_gpu_number)])
+            run_cmd = 'mpirun -np {0} --allow-run-as-root {1} -d {2}'.format(chosen_gpu_number, program_command, devices)
+        else:
+            run_cmd = program_command
+
+        run_result = self.call_program(run_cmd)
+
+        # Check that error code and error output is ok
+        if not args.parallel:
+            # MPI prints message as error for some reason
+            # So check this for serial only
+            assert run_result['stderr'] == b''
+            
+        assert run_result['returncode'] == 0
+
+        return Result(time=run_result['time'])
+
+    def save_final_config(self, configuration):
+        """called at the end of tuning"""
+        print("Optimal parameter values written to results.json:", configuration.data)
+        self.manipulator().save_to_file(configuration.data, 'results.json')
+
+
+if __name__ == '__main__':
+  argparser = opentuner.default_argparser()
+  argparser.add_argument('--problem-size', type=int, default=1, help='problem size of the program (1-4)')
+  argparser.add_argument('--gpu-num', type=int, default=1, help='number of GPUs')
+  argparser.add_argument('--parallel', type=bool, default=False, help='run on multiple GPUs')
+  MD5HashTuner.main(argparser.parse_args())
