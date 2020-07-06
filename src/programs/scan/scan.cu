ad0dfa22ec7a9f8d8e432d5e4b34f244f854c9d4
Ingunn Sund <ingunnsu@online.no>
2020-07-06 20:55:09 +0200

    Add Scan program and OpenTuner implementation

---
+++ src/programs/scan/scan.cu
@@ -0,0 +1,214 @@
+#include "cudacommon.h"
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#include "OptionParser.h"
+#include "scan.h"
+#include "scan_kernel.h"
+
+using namespace std;
+
+#define PRECISION 64
+#define BLOCK_SIZE 256
+
+// ****************************************************************************
+// Function: addBenchmarkSpecOptions
+//
+// Purpose:
+//   Add benchmark specific options parsing
+//
+// Arguments:
+//   op: the options parser / parameter database
+//
+// Returns:  nothing
+//
+// Programmer: Kyle Spafford
+// Creation: August 13, 2009
+//
+// Modifications:
+//
+// ****************************************************************************
+void addBenchmarkSpecOptions(OptionParser &op)
+{
+    op.addOption("iterations", OPT_INT, "256", "specify scan iterations");
+}
+
+// ****************************************************************************
+// Function: RunBenchmark
+//
+// Purpose:
+//   Executes the scan (parallel prefix sum) benchmark
+//
+// Arguments:
+//   resultDB: results from the benchmark are stored in this db
+//   op: the options parser / parameter database
+//
+// Returns:  nothing
+//
+// Programmer: Kyle Spafford
+// Creation: August 13, 2009
+//
+// Modifications:
+// 5/18/2011 - KS - Changing to a non-recursive algorithm
+// ****************************************************************************
+void
+RunBenchmark(OptionParser &op) {
+    int device;
+    cudaGetDevice(&device);
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, device);
+
+    #if PRECISION == 32
+        cout << "Running single precision test" << endl;
+        RunTest<float, float4>("Scan", op);
+    #else
+        cout << "Running double precision test" << endl;
+        RunTest<double, double4>("Scan-DP", op);
+    #endif
+}
+
+template <class T, class vecT>
+void RunTest(string testName, OptionParser &op)
+{
+    int probSizes[4] = { 1, 8, 32, 64 };
+
+    int size = probSizes[op.getOptionInt("size")-1];
+
+    // Convert to MiB
+    size = (size * 1024 * 1024) / sizeof(T);
+
+    // create input data on CPU
+    unsigned int bytes = size * sizeof(T);
+
+    // Allocate Host Memory
+    T* h_idata;
+    T* reference;
+    T* h_odata;
+    CUDA_SAFE_CALL(cudaMallocHost((void**) &h_idata,   bytes));
+    CUDA_SAFE_CALL(cudaMallocHost((void**) &reference, bytes));
+    CUDA_SAFE_CALL(cudaMallocHost((void**) &h_odata,   bytes));
+
+    // Initialize host memory
+    cout << "Initializing host memory." << endl;
+    for (int i = 0; i < size; i++)
+    {
+        h_idata[i] = i % 2; // Fill with some pattern
+        h_odata[i] = -1;
+    }
+
+    // Thread configuration
+    // Note: changing this may require updating the kernel calls below
+    int num_blocks  = 32;
+    int num_threads = 256;
+    // int num_blocks  = 32;
+    // int num_threads = 512;
+    int smem_size = sizeof(T) * num_threads;
+
+    // Allocate device memory
+    T* d_idata, *d_odata, *d_block_sums;
+    CUDA_SAFE_CALL(cudaMalloc((void**) &d_idata, bytes));
+    CUDA_SAFE_CALL(cudaMalloc((void**) &d_odata, bytes));
+    CUDA_SAFE_CALL(cudaMalloc((void**) &d_block_sums, num_blocks * sizeof(T)));
+
+    // Copy data to GPU
+    cout << "Copying data to device." << endl;
+    CUDA_SAFE_CALL(cudaMemcpy(d_idata, h_idata, bytes, cudaMemcpyHostToDevice));
+
+    int passes = op.getOptionInt("passes");
+    int iters = op.getOptionInt("iterations");
+
+    cout << "Running benchmark with size " << size << endl;
+    for (int k = 0; k < passes; k++) {
+        for (int j = 0; j < iters; j++) {
+            // For scan, we use a reduce-then-scan approach
+
+            // Each thread block gets an equal portion of the
+            // input array, and computes the sum.
+            reduce<T, BLOCK_SIZE><<<num_blocks, num_threads, smem_size>>>
+                (d_idata, d_block_sums, size);
+
+            // Next, a top-level exclusive scan is performed on the array
+            // of block sums
+            scan_single_block<T, BLOCK_SIZE><<<1, num_threads, smem_size*2>>>
+                (d_block_sums, num_blocks);
+
+            // Finally, a bottom-level scan is performed by each block
+            // that is seeded with the scanned value in block sums
+            bottom_scan<T, vecT, BLOCK_SIZE><<<num_blocks, num_threads, 2*smem_size>>>
+                (d_idata, d_odata, d_block_sums, size);
+        }
+        CUDA_SAFE_CALL(cudaMemcpy(h_odata, d_odata, bytes,
+                cudaMemcpyDeviceToHost));
+
+        // If results aren't correct, don't report perf numbers
+        if (! scanCPU<T>(h_idata, reference, h_odata, size))
+        {
+            return;
+        }
+    }
+    CUDA_SAFE_CALL(cudaFree(d_idata));
+    CUDA_SAFE_CALL(cudaFree(d_odata));
+    CUDA_SAFE_CALL(cudaFree(d_block_sums));
+    CUDA_SAFE_CALL(cudaFreeHost(h_idata));
+    CUDA_SAFE_CALL(cudaFreeHost(h_odata));
+    CUDA_SAFE_CALL(cudaFreeHost(reference));
+}
+
+
+// ****************************************************************************
+// Function: scanCPU
+//
+// Purpose:
+//   Simple cpu scan routine to verify device results
+//
+// Arguments:
+//   data : the input data
+//   reference : space for the cpu solution
+//   dev_result : result from the device
+//   size : number of elements
+//
+// Returns:  nothing, prints relevant info to stdout
+//
+// Programmer: Kyle Spafford
+// Creation: August 13, 2009
+//
+// Modifications:
+//
+// ****************************************************************************
+template <class T>
+bool scanCPU(T *data, T* reference, T* dev_result, const size_t size)
+{
+
+    bool passed = true;
+    T last = 0.0f;
+
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        reference[i] = data[i] + last;
+        last = reference[i];
+    }
+    for (unsigned int i = 0; i < size; ++i)
+    {
+        if (reference[i] != dev_result[i])
+        {
+#ifdef VERBOSE_OUTPUT
+            cout << "Mismatch at i: " << i << " ref: " << reference[i]
+                 << " dev: " << dev_result[i] << endl;
+#endif
+            passed = false;
+        }
+    }
+    cout << "Test ";
+    if (passed)
+        cout << "Passed" << endl;
+    else
+        cout << "---FAILED---" << endl;
+    return passed;
+}
---
+++ tuning_examples/opentuner/scan/scan_tuner.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import opentuner
+from opentuner import ConfigurationManipulator
+from opentuner import IntegerParameter
+from opentuner import EnumParameter
+from opentuner.search.manipulator import BooleanParameter
+from opentuner import MeasurementInterface
+from opentuner import Result
+from numba import cuda
+import math
+
+start_path = '../../../src/programs'
+
+class ScanTuner(MeasurementInterface):
+
+    def manipulator(self):
+        """
+        Define the search space by creating a
+        ConfigurationManipulator
+        """
+
+        gpu = cuda.get_current_device()
+        #sizes = [1000, 10000, 100000, 1000000, 10000000]
+        #numVerts = sizes[argparser.parse_args().problem_size - 1]
+
+        #min_size = 1
+        #max_size = min(numVerts, gpu.MAX_THREADS_PER_BLOCK)
+
+        manipulator = ConfigurationManipulator()
+        manipulator.add_parameter(EnumParameter('BLOCK_SIZE', [252, 252]))
+        #manipulator.add_parameter(IntegerParameter('BLOCK_SIZE', 252, 252))
+        manipulator.add_parameter(EnumParameter('PRECISION', [32, 64]))
+
+        return manipulator
+
+    def run(self, desired_result, input, limit):
+        """
+        Compile and run a given configuration then
+        return performance
+        """
+        args = argparser.parse_args()
+
+        cfg = desired_result.configuration.data
+        compute_capability = cuda.get_current_device().compute_capability
+        cc = str(compute_capability[0]) + str(compute_capability[1])
+
+        make_program = 'nvcc -gencode=arch=compute_{0},code=sm_{0} -I {1}/cuda-common -I {1}/common -g -O2 -c {1}/scan/scan.cu'.format(cc, start_path)
+        make_program += ' -D{0}={1}'.format('PRECISION',cfg['PRECISION'])
+        make_program += ' -D{0}={1} \n'.format('BLOCK_SIZE',cfg['BLOCK_SIZE'])
+
+        if args.parallel:
+            make_paralell_start = 'mpicxx -I {0}/common/ -I {0}/cuda-common/ -I /usr/local/cuda/include -DPARALLEL -I {0}/mpi-common/ -g -O2 -c -o main.o {0}/cuda-common/main.cpp \n'.format(start_path)
+            make_paralell_end = 'mpicxx -L {0}/cuda-common -L {0}/common -o BFS Graph.o main.o BFS.o bfs_kernel.o -lSHOCCommon "-L/usr/local/cuda/bin/../targets/x86_64-linux/lib/stubs" "-L/usr/local/cuda/bin/../targets/x86_64-linux/lib" -lcudadevrt -lcudart_static -lrt -lpthread -ldl -lrt -lrt'.format(start_path)
+            compile_cmd = make_paralell_start + make_program + make_paralell_end
+        else: 
+            make_serial_start = 'nvcc -I {0}/common/ -I {0}/cuda-common/ -g -O2 -c -o main.o {0}/cuda-common/main.cpp \n'.format(start_path)
+            make_serial_end = 'nvcc -L {0}/cuda-common -L {0}/common -o scan main.o scan.o -lSHOCCommon'.format(start_path)
+            compile_cmd = make_serial_start + make_program + make_serial_end
+    
+
+        compile_result = self.call_program(compile_cmd)
+        assert compile_result['returncode'] == 0
+
+        program_command = './scan -s ' + str(args.problem_size)
+        if args.parallel: 
+            chosen_gpu_number = args.gpu_num
+            if chosen_gpu_number > len(cuda.gpus):
+                chosen_gpu_number = len(cuda.gpus)
+      
+            devices = ','.join([str(i) for i in range(0, chosen_gpu_number)])
+            run_cmd = 'mpirun -np {0} --allow-run-as-root {1} -d {2}'.format(str(chosen_gpu_number), program_command, devices)
+        else: 
+            run_cmd = program_command
+
+        run_result = self.call_program(run_cmd)
+
+        # Check that error code and error output is ok
+        assert run_result['stderr'] == b''
+        assert run_result['returncode'] == 0
+
+        return Result(time=run_result['time'])
+
+    def save_final_config(self, configuration):
+        """called at the end of tuning"""
+        print("Optimal parameter values written to results.json:", configuration.data)
+        self.manipulator().save_to_file(configuration.data, 'results.json')
+
+
+if __name__ == '__main__':
+  argparser = opentuner.default_argparser()
+  argparser.add_argument('--problem-size', type=int, default=1, help='problem size of the program (1-4)')
+  argparser.add_argument('--gpu-num', type=int, default=1, help='number of GPUs')
+  argparser.add_argument('--parallel', type=bool, default=False, help='run on multiple GPUs')
+  ScanTuner.main(argparser.parse_args())
