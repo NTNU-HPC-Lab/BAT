882928376f127d081268b4b4bb5c51941900976e
Knut Kirkhorn <knutkirk@hotmail.com>
2020-08-23 20:21:46 +0200

    Add all Triad parameters for CLTune

--- src/kernels/triad/triad_kernel.cu
+++ src/kernels/triad/triad_kernel.cu
@@ -1,4 +1,11 @@
 
+// Select which precision that are used in the calculations
+#if PRECISION == 32
+    #define DATA_TYPE float
+#elif PRECISION == 64
+    #define DATA_TYPE double
+#endif
+
 // ****************************************************************************
 // Function: triad
 //
@@ -19,8 +26,29 @@
 // Modifications:
 //
 // ****************************************************************************
-extern "C" __global__ void triad(float* A, float* B, float* C, float s)
+extern "C" __global__ void triad(DATA_TYPE* A, DATA_TYPE* B, DATA_TYPE* C, DATA_TYPE s, int numberOfElements)
 {
-    int gid = threadIdx.x + (blockIdx.x * BLOCK_SIZE);
-    C[gid] = A[gid] + s*B[gid];
+    int gid = (threadIdx.x + (blockIdx.x * blockDim.x)) * WORK_PER_THREAD;
+
+    #if LOOP_UNROLL_TRIAD
+    #pragma unroll
+    #else
+    #pragma unroll(1)
+    #endif
+    for (int i = 0; i < WORK_PER_THREAD; i++) {
+        int threadId = gid + i;
+
+        // Ensure that the current thread id is less than total number of elements
+        if (threadId < numberOfElements) {
+            C[threadId] = A[threadId] + s*B[threadId];
+        }
+    }
+}
+
+extern "C" __global__ void triad_helper(float* Af, float* Bf, float* Cf, float sf, double* Ad, double* Bd, double* Cd, double sd, int numberOfElements) {
+    #if PRECISION == 32
+        triad(Af, Bf, Cf, sf, numberOfElements);
+    #elif PRECISION == 64
+        triad(Ad, Bd, Cd, sd, numberOfElements);
+    #endif
 }
--- tuning_examples/cltune/triad/reference_kernel.cu
+++ tuning_examples/cltune/triad/reference_kernel.cu
@@ -1,4 +1,11 @@
 
+// Select which precision that are used in the calculations
+#if PRECISION == 32
+    #define DATA_TYPE float
+#elif PRECISION == 64
+    #define DATA_TYPE double
+#endif
+
 // ****************************************************************************
 // Function: triad
 //
@@ -19,8 +26,30 @@
 // Modifications:
 //
 // ****************************************************************************
-extern "C" __global__ void triad(float* A, float* B, float* C, float s)
+extern "C" __global__ void triad_f(float* A, float* B, float* C, float s, int numberOfElements)
 {
     int gid = threadIdx.x + (blockIdx.x * blockDim.x);
-    C[gid] = A[gid] + s*B[gid];
+    
+    // Ensure that the current thread id is less than total number of elements
+    if (gid < numberOfElements) {
+        C[gid] = A[gid] + s*B[gid];
+    }
+}
+
+extern "C" __global__ void triad_d(double* A, double* B, double* C, double s, int numberOfElements)
+{
+    int gid = threadIdx.x + (blockIdx.x * blockDim.x);
+    
+    // Ensure that the current thread id is less than total number of elements
+    if (gid < numberOfElements) {
+        C[gid] = A[gid] + s*B[gid];
+    }
+}
+
+extern "C" __global__ void triad_helper(float* Af, float* Bf, float* Cf, float sf, double* Ad, double* Bd, double* Cd, double sd, int numberOfElements) {
+    #if PRECISION == 32
+        triad_f(Af, Bf, Cf, sf, numberOfElements);
+    #elif PRECISION == 64
+        triad_d(Ad, Bd, Cd, sd, numberOfElements);
+    #endif
 }
