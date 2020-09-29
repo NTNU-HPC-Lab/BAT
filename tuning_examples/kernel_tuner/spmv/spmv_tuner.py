9c604cde81cd96fdd64a530a5c7bbaa47ed9a8f4
Knut Kirkhorn <knutkirk@hotmail.com>
2020-09-29 22:39:03 +0200

    Minor Kernel Tuner tweak

--- tuning_examples/kernel_tuner/spmv/spmv_tuner.py
+++ tuning_examples/kernel_tuner/spmv/spmv_tuner.py
@@ -3,9 +3,15 @@ from kernel_tuner import tune_kernel, run_kernel
 from numba import cuda
 import math
 import json
+import argparse
+
+# Setup CLI parser
+parser = argparse.ArgumentParser(description="Reduction tuner")
+parser.add_argument("--size", "-s", type=int, default=1, help="problem size to the benchmark (e.g.: 2)")
+arguments = parser.parse_args()
 
 gpu = cuda.get_current_device()
-sizeIndex = 1
+sizeIndex = arguments.size
 problem_sizes = [1024, 8192, 12288, 16384]
 ellpackr_padded_sizes = [1024, 8192, 12288, 16384]
 size = problem_sizes[sizeIndex - 1]
@@ -17,17 +23,16 @@ min_block_size = 1
 max_block_size = gpu.MAX_THREADS_PER_BLOCK
 
 tune_params = dict()
-tune_params["BLOCK_SIZE"] = [i for i in range(1, max_block_size+1)]
+tune_params["BLOCK_SIZE"] = [i for i in range(min_block_size, max_block_size+1)]
 tune_params["PRECISION"] = [32, 64]
 tune_params["FORMAT"] = [0, 1, 2, 3, 4] # 0: ellpackr, 1: csr-normal-scalar, 2:  csr-padded-scalar, 3: csr-normal-vector, 4: csr-padded-vector
 tune_params["UNROLL_LOOP_1"] = [0, 1]
 tune_params["UNROLL_LOOP_2"] = [0, 1]
-tune_params["PROBLEM_SIZE"] = [sizeIndex] # For setting problem size in host code
 
 restrict = ["FORMAT < 3 or (BLOCK_SIZE == 32) or (BLOCK_SIZE == 64) or (BLOCK_SIZE == 128) or (BLOCK_SIZE == 256) or (BLOCK_SIZE == 512) or (BLOCK_SIZE == 1024)", "FORMAT > 2 or (UNROLL_LOOP_2 < 1)"]
 
 tuning_results = tune_kernel("RunBenchmark", kernel_files, size, [], tune_params, restrictions=restrict, lang="C", block_size_names=["BLOCK_SIZE"], 
-    compiler_options=["-I ../../../src/kernels/spmv/", "-I ../../../src/programs/common/", "-I ../../../src/programs/cuda-common/"])
+    compiler_options=["-I ../../../src/kernels/spmv/", "-I ../../../src/programs/common/", "-I ../../../src/programs/cuda-common/", f"-DPROBLEM_SIZE={sizeIndex}"])
 
 
 # Save the results as a JSON file
@@ -44,6 +49,9 @@ for k, v in best_parameter_config.items():
         continue
 
     best_parameters[k] = v
+
+# Add problem size to results
+best_parameters["PROBLEM_SIZE"] = sizeIndex
 
 # Save the best results as a JSON file
 with open("best-spmv-results.json", 'w') as f:
