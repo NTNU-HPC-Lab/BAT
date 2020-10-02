3b7c0ece91f6ceb00f6c24e7be48ad6523ffba2f
Knut Kirkhorn <knutkirk@hotmail.com>
2020-10-03 00:15:22 +0200

    Tweak reference checking for Triad KTT

--- tuning_examples/ktt/triad/triad_tuner.cpp
+++ tuning_examples/ktt/triad/triad_tuner.cpp
@@ -111,11 +111,13 @@ int main(int argc, char* argv[]) {
     // To set the different block sizes (local size) multiplied by the base (1)
     auto_tuner.setThreadModifier(kernelId, ktt::ModifierType::Local, ktt::ModifierDimension::X, "BLOCK_SIZE", ktt::ModifierAction::Multiply);
     // To set the different grid sizes (global size) divided by the amount of work per thread
-    auto globalModifier = [](const size_t size, const std::vector<size_t>& vector) {
+    // Divide on block size and multiply by block size after ceiling to ensure enough threads used
+    // Also use precision to get the grid size
+    // Using ceil because KTT does not ceil the divided grid size
+    auto globalModifier = [](const size_t size, const vector<size_t>& vector) {
         return int(ceil(double(size) / double(vector.at(0)) / double(vector.at(1)))) * vector.at(0) * (vector.at(2)/32*4);
     };
-    auto_tuner.setThreadModifier(kernelId, ktt::ModifierType::Global, ktt::ModifierDimension::X, 
-        std::vector<std::string>{"BLOCK_SIZE", "WORK_PER_THREAD", "PRECISION"}, globalModifier);
+    auto_tuner.setThreadModifier(kernelId, ktt::ModifierType::Global, ktt::ModifierDimension::X, {"BLOCK_SIZE", "WORK_PER_THREAD", "PRECISION"}, globalModifier);
 
     // Set reference kernel for correctness verification and compare to the computed result
     // NOTE: Due to not being able to specify the precision to match the tuned kernel, this has only been used when developing and testing this benchmark
