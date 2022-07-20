8fdf8c98dc2fe447d0c127abb78cd28dd328861a
Floris-Jan Willemsen <fjwillem@node023.localdomain>
2022-07-20 09:50:57 +0200

    Made kernel tuner reader use modifiers from specification

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 import argparse
 import logging
 
-import opentuner
-
-from src.tuners.opentuner.opentuner_runner import OpenTunerT
-from src.tuners.optuna.optuna_runner import Optuna
-
 log = logging.getLogger(__name__)
 
 
 def run_opentuner(args):
-    time = 1  # Run the tuner for 5 seconds
+    from src.tuners.opentuner.opentuner_runner import OpenTunerT
+    time = 1    # Run the tuner for 5 seconds
     args2 = args
     args2.stop_after = time
     OpenTunerT.main(args2)
 
 
 def run_optuna(args):
+    from src.tuners.optuna.optuna_runner import Optuna
     optuna_runner = Optuna()
     print(optuna_runner.main(args))
 
 
+def run_kerneltuner(args):
+    from src.tuners.kerneltuner.kerneltuner_runner import KernelTuner
+    kerneltuner_runner = KernelTuner()
+    print(kerneltuner_runner.main(args))
+
+
 runner_dict = {
     "opentuner": run_opentuner,
-    "optuna": run_optuna
+    "optuna": run_optuna,
+    "kerneltuner": run_kerneltuner,
 }
 
 
 def main():
-    parser = argparse.ArgumentParser(parents=opentuner.argparsers())
+    try:
+        import opentuner
+        parser = argparse.ArgumentParser(parents=opentuner.argparsers())
+    except ModuleNotFoundError:
+        parser = argparse.ArgumentParser()
     parser.add_argument('--testing', type=str, default=False, help='If the execution is a test or not')
-    parser.add_argument('--tuner', type=str, default=False, nargs='+', help='Which tuner to use')
-    parser.add_argument('--json', type=str, default=False, help='Path to T1-compliant JSON')
+    parser.add_argument('--tuner', type=str, default=['kerneltuner'], nargs='+', help='Which tuner to use')
+    parser.add_argument('--json', type=str, default="src/benchmarks/MD5Hash/MD5Hash-CAFF.json", help='Path to T1-compliant JSON')
     parser.add_argument('--benchmark', type=str, default=False, nargs='+', help='Which benchmarks to run')
+    parser.add_argument('--gpu_name', type=str, default='A4000', help='The CUDA GPU to run on')
     args = parser.parse_args()
 
     for tuner in args.tuner:
--- src/tuners/kerneltuner/kerneltuner_runner.py
+++ src/tuners/kerneltuner/kerneltuner_runner.py
@@ -1,27 +1,52 @@
 from __future__ import print_function
 
 from builtins import str
+import ast
 
 import kernel_tuner
 from pathlib import Path
 from collections import OrderedDict
 
-from src.reader import reader, T1_specification
+from src.readers.python.cuda.cupy_reader import CupyReader
 
 
 class KernelTuner:
 
     def main(self, args):
         self.prog_args = args
-        self.spec = T1_specification.get_spec(args.json)
+        self.reader = CupyReader(args.json)
+        self.spec = self.reader.spec
         strategy_options = dict(popsize=0, max_fevals=10)
-        self.tune(args.device_name, strategy_options=strategy_options)
+        self.tune(args.gpu_name, strategy_options=strategy_options)
 
-    # def make_gridsize_lambda(x, y, z):
+    def problemsize_from_gridsizes(self, gridsizes: dict):
+        """ Takes the grid sizes and returns the problem size as a lambda function """
+        problemsizes = list()
+        dimensions = ['X', 'Y', 'Z']    # order in which Kernel Tuner expects the tuple
+        for dimension in dimensions:
+            if dimension not in gridsizes:
+                break
+            gridsize = gridsizes[dimension]
+            # not an expression, so must be a scalar
+            if not isinstance(gridsize, str):
+                problemsizes.append(gridsize)
+                continue
+            # gridsize is expression, so find and wrap the variables
+            paramnames = [node.id for node in ast.walk(ast.parse(gridsize)) if isinstance(node, ast.Name)]
+            for paramname in paramnames:
+                gridsize = gridsize.replace(paramname, f"p['{paramname}']")
+            problemsizes.append(gridsize)
 
-    def tune(self, device_name, strategy="mls", strategy_options=None, verbose=True, quiet=False, simulation_mode=False):
+        # check if the strict use of X, Y, Z causes problems
+        if len(problemsizes) != len(gridsizes.keys()):
+            raise ValueError(f"The problem size dimensions ({dimensions}) do not match the gridsizes specified ({gridsizes})")
+
+        # return the lambda function for future evaluation
+        return lambda p: tuple(eval(ps, dict(p=p)) if isinstance(ps, str) else ps for ps in problemsizes)
+
+    def tune(self, gpu_name, strategy="mls", strategy_options=None, verbose=True, quiet=False, simulation_mode=False):
         config_space = self.spec["configurationSpace"]
-        kernel_spec = self.spec["kernelSpecification"]
+        kernel_spec = self.reader.kernel_spec
         kernel_name = kernel_spec["kernelName"]
 
         # read in kernel
@@ -31,14 +56,13 @@ class KernelTuner:
             kernel_string += fp.read()
 
         # get arguments
-        args = T1_specification.populate_args(kernel_spec)
+        args = self.reader.populate_args()
 
         # get problem-, block-, thread-, and grid sizes
-        problem_size = tuple(s for s in kernel_spec["gridSize"].values())
-        problem_size = lamda p: (73728//p["BLOCK_SIZE"]//p["WORK_PER_THREAD"], 1, 1)
-        block_size_names = list(n for n in kernel_spec["blockSize"] if isinstance(n, str))
-        grid_div_x = None
-        grid_div_y = None
+        problem_size = self.problemsize_from_gridsizes(kernel_spec["gridSize"])
+        block_size_names = list(n for n in kernel_spec["blockSize"].values() if isinstance(n, str))
+        grid_div_x = []
+        grid_div_y = []
         # mods = kernel_spec["modifiers"]
         # if mods and mods["type"] == "block_size" and mods["action"] == 'divide':
         #     if mods["dimension"] == "X":
@@ -48,20 +72,17 @@ class KernelTuner:
         #     else:
         #         print(f"Unkown modifier dimension {mods['dimension']}")
 
-        # get grid divisors
-        # can be callable!
-
         # add tune params
         tune_params = OrderedDict()
         for param in config_space["tuningParameters"]:
             tune_params[param["name"]] = eval(str(param["values"]))
 
         # add restrictions
-        restrict = []
-        # for constraint in self.spec["constraints"]:
-        #     restrict += constraint
+        if "constraints" in self.spec:
+            for constraint in self.spec["constraints"]:
+                restrict += constraint
 
         results, env = kernel_tuner.tune_kernel(kernel_name, kernel_string, problem_size, args, tune_params, lang='cupy', block_size_names=block_size_names,
                                                 restrictions=restrict, verbose=verbose, quiet=quiet, grid_div_x=grid_div_x, grid_div_y=grid_div_y, device=0,
-                                                platform=0, iterations=32, cache="BAT_" + kernel_name + "_" + device_name, strategy=strategy,
+                                                platform=0, iterations=32, cache="BAT_" + kernel_name + "_" + gpu_name, strategy=strategy,
                                                 strategy_options=strategy_options, simulation_mode=simulation_mode)
