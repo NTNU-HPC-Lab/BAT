48f007efc55fc7e2a4178f87950d122475196e0b
Jacob Odgård Tørring <jacob.torring@gmail.com>
2022-10-05 23:42:23 +0200

    Restructures data and metadata paths and adds write interval for data

--- src/result/dataset.py
+++ src/result/dataset.py
@@ -7,17 +7,26 @@ class Dataset:
     def __init__(self, spec):
         self.spec = spec
         self.search_settings = self.spec["SearchSettings"]
-        ext = self.spec["General"]["OutputFormat"].lower()
-        if ext not in ("json", "hdf5"):
-            raise Exception("Invalid output format", ext)
-        self.filename = "{}-{}-results.{}".format(self.search_settings["TunerName"], self.search_settings["Trials"], ext)
+        self.ext = self.spec["General"]["OutputFormat"]
+        self.benchmark_name = self.spec["General"]["BenchmarkName"]
+        if self.ext not in ("JSON", "HDF5"):
+            raise Exception("Invalid output format", self.ext)
+        self.filename_root = "{}-{}-{}".format(self.search_settings["TunerName"], self.benchmark_name, self.search_settings["Trials"], self.ext)
+        self.filename_data = "{}-results.{}".format(self.filename_root, self.ext.lower())
+        self.filename_metadata = "{}-metadata.json".format(self.filename_root)
+        self.data_path = "./results/{}".format(self.filename_data)
+        self.metadata_path = "./results/{}".format(self.filename_metadata)
         self.results = []
+        self.write_interval = 10
 
     def add_result(self, result):
         self.results.append(result)
+        if len(self.results) % self.write_interval == 0:
+            self.write_data()
 
-    @staticmethod
-    def write_metadata():
+
+
+    def write_metadata(self):
         metadata = {}
         nvidia_smi_out = subprocess.run(["nvidia-smi", "--query", "-x"], capture_output=True)
         o = xmltodict.parse(nvidia_smi_out.stdout)
@@ -26,7 +35,7 @@ class Dataset:
 
         lshw_out = subprocess.run(["lshw", "-json"], capture_output=True)
         metadata["lshw"] = json.loads(lshw_out.stdout)
-        with open('./results/optuna/{}'.format('metadata.json'), 'w') as f:
+        with open(self.metadata_path, 'w') as f:
             metadata_json = json.dumps(metadata, indent=4)
             f.write(metadata_json)
 
@@ -35,16 +44,11 @@ class Dataset:
         for result in self.results:
             dump_results["results"].append(result.serialize())
 
-        ext = self.filename.split(".")[-1]
-        if ext == "json":
-            with open("./results/optuna/{}".format(self.filename), 'a') as f:
+        if self.ext == "JSON":
+            with open(self.data_path, 'w') as f:
                 json.dump(dump_results, f, indent=4)
-        elif ext == "hdf5":
+        elif self.ext == "HDF5":
             df = pd.json_normalize(dump_results["results"])
-            print(df)
-            df.to_hdf(self.filename, key="Results", mode="w", complevel=9)
+            df.to_hdf(self.data_path, key="Results", mode="w", complevel=9)
         else:
-            print("Unsupported file extention", ext)
-
-if __name__ == "__main__":
-    Dataset.write_metadata()
+            print("Unsupported file extention", self.ext)
