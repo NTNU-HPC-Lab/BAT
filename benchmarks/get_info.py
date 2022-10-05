2759f3bb27594af2a216d940b71c97f2d75380dc
Jacob Odgård Tørring <jacob.torring@gmail.com>
2022-10-05 23:41:00 +0200

    Refactors benchmark info script

--- benchmarks/get_info.py
+++ benchmarks/get_info.py
@@ -1,24 +1,28 @@
 import os
 import json
 
-benchmarks = []
-for folder_name in os.listdir("."):
-    try:
-        with open("./{folder_name}/{folder_name}-CAFF.json".format(folder_name=folder_name), 'r') as f:
-            benchmark = {}
-            text = f.read()
-            j = json.loads(text)
-            benchmark["name"] = j["General"].get("BenchmarkName")
-            tuning_params = j["ConfigurationSpace"]["TuningParameters"]
-            benchmark["dimensionality"] = len(tuning_params)
-            cardinality = 1
-            for param in tuning_params:
-                cardinality *= len(eval(str(param["Values"])))
-            benchmark["cardinality"] = cardinality
-            benchmarks.append(benchmark)
-    except:
-        pass
+def main():
+    benchmarks = []
+    for folder_name in os.listdir("."):
+        try:
+            with open("./{folder_name}/{folder_name}-CAFF.json".format(folder_name=folder_name), 'r') as f:
+                benchmark = {}
+                text = f.read()
+                j = json.loads(text)
+                benchmark["name"] = j["General"].get("BenchmarkName")
+                tuning_params = j["ConfigurationSpace"]["TuningParameters"]
+                benchmark["dimensionality"] = len(tuning_params)
+                cardinality = 1
+                for param in tuning_params:
+                    cardinality *= len(eval(str(param["Values"])))
+                benchmark["cardinality"] = cardinality
+                benchmarks.append(benchmark)
+        except:
+            pass
 
-for benchmark in sorted(benchmarks, key=lambda item: item["cardinality"], reverse=True):
-    print("Benchmark: {0:15} Dimensionality: {1:3}\t Cardinality: {2:,}".format(benchmark["name"], benchmark["dimensionality"], benchmark["cardinality"]))
+    for benchmark in sorted(benchmarks, key=lambda item: item["cardinality"], reverse=True):
+        print("Benchmark: {0:15} Dimensionality: {1:3}\t Cardinality: {2:,}".format(benchmark["name"], benchmark["dimensionality"], benchmark["cardinality"]))
+
+if __name__ == "__main__":
+    main()
 
