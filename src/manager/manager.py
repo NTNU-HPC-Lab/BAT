30f1cd588dea899846d6489e5342fb146a55fecc
Jacob Odgård Tørring <jacob.torring@gmail.com>
2022-10-16 14:38:22 +0200

    Refactors interface to accept search-settings.json seperately from spec

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -3,12 +3,60 @@ import os
 
 from src.config_space import ConfigSpace
 from src.result.dataset import Dataset
-from src.manager.util import get_spec
+from src.manager.util import get_spec, get_search_spec
+
+
+class ExperimentManager:
+    def __init__(self):
+        self.runner_dict = {
+            "opentuner": self.run_opentuner,
+            "optuna": self.run_optuna,
+            "kerneltuner": self.run_kerneltuner,
+            "smac": self.run_smac,
+            "smac3": self.run_smac
+        }
+    @staticmethod
+    def run_opentuner(args):
+        from src.tuners.opentuner_runner import OpenTunerT
+        print(OpenTunerT.main(args))
+
+    @staticmethod
+    def run_smac(args):
+        from src.tuners.smac3_runner.smac3_runner import SMAC3
+        print(SMAC3().main(args))
+
+    @staticmethod
+    def run_optuna(args):
+        from src.tuners.optuna_runner import Optuna
+        print(Optuna().main(args))
+
+    @staticmethod
+    def run_kerneltuner(args):
+        from src.tuners.kerneltuner_runner import KernelTuner
+        print(KernelTuner().main(args))
+
+    def start(self, args):
+        self.search_spec = get_search_spec()
+        benchmarks = args.benchmarks if args.benchmarks else self.search_spec["BenchmarkConfig"]["Benchmarks"]
+
+        if benchmarks[0].lower() == "all":
+            benchmarks = ["GEMM","nbody", "MD5Hash", "FFT", "TRIAD"]
+
+        tuner = args.tuner if args.tuner else self.search_spec["SearchSettings"]["TunerName"]
+
+        for benchmark in benchmarks:
+            args.json = "./benchmarks/{}/{}-CAFF.json".format(benchmark, benchmark)
+            if tuner is not None:
+                print("Running {} with {}".format(tuner, args))
+                self.runner_dict[tuner](args)
+
+
 
 class Manager:
     def __init__(self, args):
         self.root_results_path = "./results"
         self.spec = get_spec(args.json)
+        self.search_spec = get_search_spec()
         self.validate_schema(self.spec)
 
         self.config_space = ConfigSpace(self.spec["ConfigurationSpace"])
@@ -17,12 +65,14 @@ class Manager:
         lang = self.spec["KernelSpecification"]["Language"]
         if lang == "CUDA":
             from src.cuda_kernel_runner import CudaKernelRunner, ArgHandler
-            self.runner = CudaKernelRunner(self.spec, self.config_space)
+            self.runner = CudaKernelRunner(self.spec, self.config_space, self.search_spec)
             self.arg_handler = ArgHandler(self.spec)
 
 
         #self.filename = "optuna-results.hdf5"
         self.testing = 0
+
+
 
     def validate_schema(self, spec):
         from jsonschema import validate
