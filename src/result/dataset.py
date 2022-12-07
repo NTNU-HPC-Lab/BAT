18c3597f54cdcf628fc4d65a9f9d2332e04e93f1
Jacob Odgård Tørring <jacob.torring@gmail.com>
2022-12-07 15:29:01 +0100

    Adds more metadata based on SC reproducibility script. Fixes hash of metadata to be deterministic again.

--- src/result/dataset.py
+++ src/result/dataset.py
@@ -1,12 +1,14 @@
 import subprocess
-import json
-import hashlib
-import shutil
 import os
 import copy
 from pathlib import Path
-import xmltodict
+
 import pandas as pd
+import json
+import hashlib
+import shutil
+import xmltodict
+import jc
 
 from src.manager.util import get_spec, get_kernel_path
 from src.result.zenodo import Zenodo
@@ -68,22 +70,25 @@ class Dataset:
         shutil.copyfile(self.kernel_path, f"{self.path}/source/kernel.cu")
         self.files.append(self.source_zip)
 
+
+
     def create_dataset_folder(self):
         # Create dataset folder
         self.root_path = "./results"
         hash_set = copy.deepcopy(self.spec)
-        hash_set.update(self.metadata)
+        copy_metadata = copy.deepcopy(self.metadata)
+        hash_set.update(copy_metadata)
         del hash_set["General"]
         del hash_set["zenodo"]
 
-        lshw = hash_set["environment"]["lshw"]
-        hostname = lshw[0]["id"] if isinstance(lshw, list) else lshw["id"]
-
         del hash_set["environment"]["lshw"]
-        hash_set["environment"]["hostname"] = hostname
+        del hash_set["environment"]["lscpu"]
+        del hash_set["environment"]["meminfo"]
+        hash_set["environment"]["hostname"] = Dataset.get_hostname()
         # del hash_set["environment"]["lshw"][0]["children"][0]["children"][1]["size"] TODO: This is not consistent across systems
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["timestamp"]
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["fan_speed"]
+        del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["clocks_throttle_reasons"]
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["performance_state"]
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["fb_memory_usage"]
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["bar1_memory_usage"]
@@ -93,13 +98,21 @@ class Dataset:
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["clocks"]
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["applications_clocks"]
         del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["default_applications_clocks"]
+        del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["pci"]["rx_util"]
+        del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["pci"]["tx_util"]
+        del hash_set["environment"]["nvidia_query"]["nvidia_smi_log"]["gpu"]["pci"]["pci_gpu_link_info"]["pcie_gen"]["current_link_gen"]
 
-        self.hash = hashlib.md5(json.dumps(hash_set,
+        json_hash_set = json.dumps(hash_set,
             ensure_ascii=False,
             sort_keys=True,
-            indent=None,
+            indent=4,
             separators=(',', ':'),
-        ).encode('utf-8')).hexdigest()
+        )
+
+        with open("test", 'w') as f:
+            f.write(json_hash_set)
+
+        self.hash = hashlib.md5(json_hash_set.encode('utf-8')).hexdigest()
         self.path = f"{self.root_path}/{self.hash}"
         Path(self.path).mkdir(parents=True, exist_ok=True)
 
@@ -127,6 +140,10 @@ class Dataset:
         self.files.append(metadata_filename)
 
     @staticmethod
+    def get_hostname():
+        return subprocess.run(["hostname"], capture_output=True).stdout.decode("utf-8")
+
+    @staticmethod
     def get_metadata():
         metadata = {}
         metadata["zenodo"] = Zenodo.get_zenodo_metadata()
@@ -148,15 +165,33 @@ class Dataset:
         o = xmltodict.parse(nvidia_smi_out.stdout)
         del o["nvidia_smi_log"]["gpu"]["processes"]
         metadata["nvidia_query"] = o
-        lshw_out = subprocess.run(["lshw", "-json"], capture_output=True)
+        lshw_out = subprocess.run(["lshw", "-json", "-sanitize"], capture_output=True)
         metadata["lshw"] = json.loads(lshw_out.stdout)
+        lscpu_out = subprocess.run(["lscpu", "--json"], capture_output=True)
+        metadata["lscpu"] = json.loads(lscpu_out.stdout)
+        proc_out = subprocess.run(["cat", "/proc/meminfo"], capture_output=True)
+        metadata["meminfo"] = jc.parse('proc_meminfo', proc_out.stdout.decode("utf-8"))
+        lsblk_out = subprocess.run(["lsblk", "-a"], capture_output=True)
+        metadata["lsblk"] = jc.parse('lsblk', lsblk_out.stdout.decode("utf-8"))
+
         return metadata
+
+    @staticmethod
+    def get_lsb_release():
+        lsb_release_out = subprocess.run(["lsb_release", "-a"], capture_output=True)
+        r = {}
+        for line in lsb_release_out.stdout.decode("utf-8").split("\n"):
+            line_list = line.split("\t")
+            r[line_list[0]] = line_list[-1]
+        return r
+
 
     @staticmethod
     def get_environment_metadata():
         env_metadata = {}
         env_metadata["requirements"] = Dataset.save_requirements()
         env_metadata = Dataset.get_hardware_metadata(env_metadata)
+        env_metadata["lsb_release"] = Dataset.get_lsb_release()
         return env_metadata
 
     def write_data(self):
