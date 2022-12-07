710314afde6ef705ade08054a45bbd80e1629427
Jacob Odgård Tørring <jacob.torring@gmail.com>
2022-12-07 15:31:08 +0100

    Fixes KTT_setup.sh to correctly compile KTT's TuningLancher

--- src/tuners/KTT_runner/KTT_setup.sh
+++ src/tuners/KTT_runner/KTT_setup.sh
@@ -1,4 +1,3 @@
-source environment.sh
-./premake5 gmake
-cd Build && make config=release_x86_64 --python
+./premake5 --platform=nvidia --python --tuning-loader gmake
+cd Build && PYTHON_HEADERS=/usr/include/python3.8 PYTHON_LIB=/lib/x86_64-linux-gnu/libpython3.8.so make config=release_x86_64
 
--- src/tuners/KTT_runner/environment.sh
+++
@@ -1,2 +0,0 @@
-export PYTHON_HEADERS="/usr/include/python3.8/"
-export PYTHON_LIB="/usr/local/lib/python3.8/"
