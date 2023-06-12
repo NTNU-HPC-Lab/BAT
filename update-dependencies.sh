2268495f2d5414a2af5a231a748b26cfdef317a3
Jacob Odgård Tørring <jacob.torring@gmail.com>
2023-06-12 08:06:44 -0700

    Remove extra files

--- update-dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pipreqs --force --savepath $1
-pip3 freeze | grep "pipreqs" >> $1
--- zenodo-example.py
+++
@@ -1,9 +0,0 @@
-import os
-from src.result.zenodo import Zenodo
-
-if __name__ == "__main__":
-    datasets = os.listdir("./results")
-    z = Zenodo(datasets)
-    z.upload()
-    # WARNING!: DO NOT UNCOMMENT THIS LINE UNLESS YOU FULLY UNDERSTAND WHAT IT DOES:  z.delete_all()
-
